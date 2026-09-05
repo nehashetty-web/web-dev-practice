187c2246a98713b467354d9437730eaddeeef4e9
nehashetty-web <nehasree542@gmail.com>
2026-09-05 22:31:04 +0530

    Update react.js

---
+++ HTML/react.js
@@ -0,0 +1,18 @@
+function App(){
+    return(
+        <div>
+            <h1> my first react app</h1>
+            <p>learning react</p>
+            <button>click</button>
+        </div>
+    );
+}
+function Details(){
+    return(
+        <div>
+            <h1>my name is neha</h1>
+            <p>Course: B.Tech CSE-DS</p>
+            <p>Learning: React</p>
+        </div>
+    );
+}
