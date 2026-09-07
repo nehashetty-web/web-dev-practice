efb804bcc44b36e8772129343a2bae58e06f2f61
nehashetty-web <nehasree542@gmail.com>
2026-09-07 05:53:52 +0530

    Update react.js

--- HTML/react.js
+++ HTML/react.js
@@ -34,3 +34,17 @@ function App(){
         </div>
     );
 }
+
+"state and usestate"
+import{useState} from "react";
+function App(){
+    const[count,setCount]=useState(0);
+    return(
+        <div>
+            <h1>count</h1>
+        <button onClick={() => setCount(count+1)}>
+            Increase
+        </button>
+        </div>
+    );
+}
