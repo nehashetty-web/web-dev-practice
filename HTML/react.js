6b3234d60e5900d7e90a0a0bbb0ac68c9247b0d4
nehashetty-web <nehasree542@gmail.com>
2026-09-21 23:31:29 +0530

    Update react.js

--- HTML/react.js
+++ HTML/react.js
@@ -128,3 +128,15 @@ function About() {
 
 function App() {
   return (
+    <BrowserRouter>
+
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+
+    </BrowserRouter>
+  );
